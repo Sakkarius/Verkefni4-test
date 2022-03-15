26bf916d01a82dec918ce266a6f36d1e4fac1d13
Sakkarius <101215611+Sakkarius@users.noreply.github.com>
2022-03-15 17:03:02 +0000

    Verkefni4.1
    Fyrsti partur af verkefni 4

--- bin/Test33.dart
+++ bin/Test33.dart
@@ -1,4 +1,12 @@
 
 void main(List<String> arguments) {
-  print("test");
+  //Verkefni 1
+
+  List<int> calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+  int sum = 0;
+  for (int i = 0; i < calculate_sum.length; i++)
+    {
+      sum = sum+calculate_sum[i];
+    }
+  print(sum);
 }
